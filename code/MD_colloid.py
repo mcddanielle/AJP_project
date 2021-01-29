17eaa52f2243b19245f83acd9c9026380088a6d8
mcddanielle <mcd.danielle@gmail.com>
2021-01-29 08:11:49 -0800

    brownian motion code

--- code/MD_colloid.py
+++ code/MD_colloid.py
@@ -18,6 +18,27 @@ import matplotlib.gridspec as gridspec
 #set default font of plot
 plt.rc('font', size=24)
 
+
+##################################################################
+def brownian_distribution(parameters):
+    '''
+    '''
+    mu, sigma = 0.0, 1.0 # mean and standard deviation
+
+    #temperature has units of force too, because of course it does
+    kb_T = parameters['temperature']
+
+    timesteps = int(parameters['maxtime'])
+    
+    #use numpy's built-in function to generate a randomized force
+    #do so in bulk for every timestep in the simulation
+    #scale the prefactor by 2 \eta kb T in simulation units    
+    fy_rand = kb_T * np.random.normal(mu, sigma, timesteps)
+
+    return fy_rand
+    
+    #fx_rand = np.random.normal(mu, sigma, int(N_part*(N_part-1)))
+    
 ##################################################################
 def landscape_force(y,parameters):
     '''landscape potential is equation (1) in the manuscript, while force is equation (5) - for now anyway
@@ -94,12 +115,15 @@ def average_velocity(int_time, vy, avg_vy, parameters):
 
 
 #####################################################################
-def md_step(y, int_time, F_DC, avg_vy, parameters):
+def md_step(y, int_time, F_DC, avg_vy, parameters, ft=0):
     '''
+
+    ft random kicks due to temperature affects
     '''
 
     dt = parameters['dt']
     SY = parameters['Sy']
+    temperature = parameters['temperature']
 
     time = int_time * dt
     
@@ -110,6 +134,9 @@ def md_step(y, int_time, F_DC, avg_vy, parameters):
 
     #driving force
     vy += external_drive(F_DC, time, parameters)
+
+    if ft != 0:
+        vy += ft
 
     #calculate the average velocity over all vy values
     avg_vy = average_velocity(int_time, vy, avg_vy, parameters)
@@ -129,6 +156,47 @@ def md_step(y, int_time, F_DC, avg_vy, parameters):
         y += SY
     
     return y, vy, avg_vy
+
+
+#####################################################################
+def plot_position_vs_time(time_data,y_data,p):
+    '''Make Fig. 2 in AJP
+    '''
+
+    #for plot
+    #calculate the driving force from F_DC and time
+    F_AC = p['F_AC']
+    freq = p['freq']
+    period = p['period']
+    
+    fig = plt.figure(figsize=(8,8))
+    gs=gridspec.GridSpec(1,1)
+    ax2 = fig.add_subplot(gs[0,0])  #scatter plot of particles
+
+    ax2.plot(time_data,y_data/period,lw=5) #,'o--') #,markevery=100)
+    #plt.xlim(155,157)
+    ax2.set_xlabel(r"time ($\tau$)")
+    ax2.set_ylabel(r"y/$\lambda$")
+
+    ax2.set_xlim(0,time_data[-1]+1)
+    #ax2.set_ylim(-0.1,y_data[-1]/period+0.2)
+
+    #ax2.text(0.9,0.84,"(b)",transform = ax2.transAxes,backgroundcolor="white",zorder=-10)
+    
+    #add horizontal lines for potential minima
+    '''
+    for i in [1,3,5,7]:
+        ax2.axhline(y=i, color = "black", linestyle = "--")
+
+    for i in [1,5,9,13]:
+        ax2.axvline(x=i/(4*freq), color = "black", linestyle = "--") 
+    '''
+    
+    plt.tight_layout(pad=0.1,h_pad=-0.3)
+    #g.tight_layout(fig, rect=[0, 0, 1, 1], h_pad=p_val,w_pad=p_val,pad=0.0)
+    plt.savefig(parameters['filename'])
+    
+    return 
 
 
 #####################################################################
@@ -160,7 +228,7 @@ def plot_force_position_vs_time(time_data,FDC_data,y_data,p):
     #ax1.legend(loc=4,fontsize=20,borderaxespad=0.0,frameon=0,handlelength=1.5) #,labelspacing=0.2
     ax2.plot(time_data,y_data/period,lw=5) #,'o--') #,markevery=100)
     #plt.xlim(155,157)
-    ax2.set_xlabel("time (\tau)")
+    ax2.set_xlabel(r"time ($\tau$)")
     ax2.set_ylabel(r"y/$\lambda$")
     ax1.set_ylabel(r"F(t)")
 
@@ -316,8 +384,83 @@ def single_particle(parameters,plot="y-position"):
         return np.average(avg_vy_data) #/decifactor
       
 
+def brownian_particle(parameters):
+    '''
+    '''
 
+    #define the constants (parameters and initial conditions)
+    dt = parameters['dt']
+    vy = parameters['vy0']
+    avg_vy = 0 #vy
+    time = parameters['time0']
 
+    #start the particle in a local minima
+    y = parameters['y0']
+
+    #assume no applied force for now
+    F_DC = 0.0 #parameters['F_DC']
+    F_DC_incr = 0.0 #parameters['F_DC_incr']
+    F_DC_max = 0.0 #parameters['F_DC_max']
+    parameters['F_AC']=0.0
+    
+    #integer time steps
+    maxtime=parameters['maxtime']
+    writemovietime=parameters['writemovietime']
+    
+    #define arrays to hold data as a function of time
+    array_length = int(maxtime/writemovietime)
+    y_data = np.zeros(array_length)
+    #FDC_data = np.zeros(array_length)
+    vy_data = np.zeros(array_length)
+    time_data = np.zeros(array_length)
+
+    avg_vy_data = np.zeros(int(maxtime/parameters['decifactor']))
+    #avg_FDC_data = np.zeros(int(maxtime/parameters['decifactor']))
+
+    #array should be the same length as above
+    fy_rand = brownian_distribution(parameters)
+    
+    #loop through the integer time steps in the simulation
+    for int_time in range(0,maxtime):
+
+        #increase F_DC if needed
+        #if int_time > 0 and abs(F_DC - F_DC_max) > F_DC_incr: 
+        #     F_DC = ramp_dc_force(F_DC,int_time, parameters)
+
+        #apply the force calculations for the current position/time
+        #note vy is not necessary
+
+        y, vy, avg_vy = md_step(y, int_time,
+                                F_DC, avg_vy, parameters, ft=fy_rand[int_time])
+        
+        if int_time % writemovietime == 0:
+            #print(int_time)
+            #update the data
+            i = int(int_time/writemovietime)
+            y_data[i] = y
+            vy_data[i] = vy
+            #FDC_data[i] = F_DC
+            time_data[i] = time
+
+        if int_time % parameters['decifactor'] == 0:
+            j = int(int_time/parameters['decifactor'])
+
+            #print(int_time,len(avg_vy_data))
+            avg_vy_data[j] = avg_vy
+
+            #print(int_time,avg_vy_data[j])
+
+            #avg_FDC_data[j] = F_DC
+
+            #reset the running average
+            avg_vy = 0
+
+        #update the time (simulation units)
+        time += dt
+
+    plot_position_vs_time(time_data,y_data,parameters)
+    return
+    
     
 #########################################################################
 def set_parameters():
@@ -335,13 +478,16 @@ def set_parameters():
     dict['time0'] = 0       #initial time in integer timesteps
 
     #start the particle in a local minima (not a local max!)
-    dict['y0'] = 0. #period  #Sy/2
+    dict['y0'] =  dict['Sy']/2
 
     #landscape potential
     dict['F0'] = 0.1
     dict['Np'] = 20         #number of troughs in the substrate
     dict['period'] = dict['Sy']/dict['Np']  #spatial period of substrate in y-direction
 
+    #Brownian motion factor
+    dict['temperature'] = 5.7*dict['F0']
+    
     #integer time steps
     dict['maxtime']=400000        #total time steps in simulation
     dict['writemovietime']=1000   #interval to write data to arrays for plotting
@@ -374,19 +520,37 @@ if __name__ == "__main__":
 
     parameters = set_parameters()
 
-    make_fig2 = True #False
+    make_fig2 = False #True 
     make_fig3 = False #True
+    make_brownian = True
 
     #--------------------------------------------------------------
     #make Fig. 2 - run a single particle at a single driving force
     #--------------------------------------------------------------
 
     if make_fig2:
-        parameters['filename']="single_particle.pdf"
+        
+        parameters['dt'] = 0.1 #timestep in simulation units
+        parameters['maxtime']=4000        #total time steps in simulation
+        parameters['writemovietime']=1   #interval to write data to arrays for plotting
+        parameters['decifactor']=1   #interval to write data to arrays for plotting
+
+        parameters['filename']="single_particle_dt0.1.pdf"
 
         #run a single MD simulation for a set of parameters
         single_particle(parameters)
 
+    if make_brownian:
+        
+        parameters['dt'] = 0.1 #timestep in simulation units
+        parameters['maxtime']=10000000        #total time steps in simulation
+        parameters['writemovietime']=10000   #interval to write data to arrays for plotting
+        parameters['decifactor']=1   #interval to write data to arrays for plotting
+        parameters['filename']="brownian_particle_dt0.1.pdf"
+
+        #run a single MD simulation for a set of parameters
+        brownian_particle(parameters)
+        
     if make_fig3: 
         #reset some parameters for subsequent figures
         #parameters['F_DC_incr'] = 0.01        #amount to increase FDC at every drop step
