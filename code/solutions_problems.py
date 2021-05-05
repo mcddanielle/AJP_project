9da0b41146b7dc6d439eab3eb27903328e4e4b53
mcddanielle <mcd.danielle@gmail.com>
2021-05-05 07:40:29 -0700

    moved ex. 3 to solutions

--- code/solutions_problems.py
+++ code/solutions_problems.py
@@ -6,7 +6,7 @@ Supplementary Material for
 Molecular dynamics simulation of synchronization in driven particles
 American Journal of Physics
 
-Solutions to Problems 1-6
+Solutions to Problems 2,3,5,6
 
 Danielle McDermott
 Tiare Guerrero
@@ -183,6 +183,34 @@ if __name__ == "__main__":
                     
         plt.tight_layout(pad=0.2)
         plt.savefig(parameters['filename'])
+
+    #--------------------------------------------------------------
+    #calculate the approximate Reynolds number
+    #at standard temperature and pressure, converted to SI units (i.e. m-kg-sec)
+
+    if solve_problem == "3":
+
+        D = 1e-6 #micrometer
+        rho = 0.9982 * 1e-3 * 1e6 #g/cm^3  1g=10^-3kg, 1/cm^3*(100cm/1m)^3
+        v = 1e-6 #micrometer / sec
+
+        #"kinematic viscosity" - not the right units
+        #eta = 0.01 #cm^2 / sec
+
+        #"dynamics viscosity" - same units as Taylor
+        eta = 1.0016e-3 #mPa *sec = mN/m^2 *sec
+
+
+        #unit check:  m*m/sec*kg/(m^3)*1/(N/m^2*sec)
+        #blech! 
+        #unit check:  kg/m * m^2 / (N*sec^2) 
+        #unit check:  m*kg / (N*sec^2) = m*kg / (kg*m s^2 /s^2)
+        #kg cancels!  m cancels! s^2 cancels!
+        #unitless Reynold's number
+
+
+        R = D*v*rho/eta
+        print("The Reynolds number is: ",R)
         
     #--------------------------------------------------------------
     #study Brownian motion for increasing temperature
